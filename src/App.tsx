7f76b36cee86e9e69b73e3b7e22a2be95d24efb3
Matt Evans <mattevans98@gmail.com>
2023-03-23 12:58:00 -0500

    Added togglable scenario displays

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import './App.scss';
+import scenario1 from "./scenario1/Scenario1";
+import Scenario1 from "./scenario1/Scenario1";
+import Scenario2 from "./scenario2/Scenario2";
 
 const App = (): ReactElement => {
+    const [scenario, setScenario] = useState(-1);
+    const scenarioArr: ReactElement[] = [<Scenario1/>, <Scenario2/>];
+
     return (
         <div className="App">
             <header className="App-header">
@@ -11,13 +17,13 @@ const App = (): ReactElement => {
                 <div className="scenario-selector">
                     <h3>Select a scenario</h3>
                     <div className="scenario-buttons">
-                        <button>Scenario 1</button>
-                        <button>Scenario 2</button>
+                        <button onClick={ () => setScenario(scenario !== 0 ? 0 : -1) }>Scenario 1</button>
+                        <button onClick={ () => setScenario(scenario !== 1 ? 1 : -1) }>Scenario 2</button>
                         <button>Scenario 3</button>
                         <button>Scenario 4</button>
                     </div>
                     <div className="scenario-display">
-
+                        { scenario === -1 ? <></> : scenarioArr[0] }
                     </div>
                 </div>
             </main>
