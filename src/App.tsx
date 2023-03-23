ac3bc2c5b920f21501d095dcaec2cf845bb32829
Matt Evans <mattevans98@gmail.com>
2023-03-23 12:03:15 -0500

    Added Scenario2 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import './App.css';
+import React, { ReactElement } from 'react';
+import './App.scss';
 
-function App() {
+function App(): ReactElement {
     return (
         <div className="App">
             <header className="App-header">
