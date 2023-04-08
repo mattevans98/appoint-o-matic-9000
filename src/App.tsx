c99f7e8bceb2ae72689284ed2259d151ef9fed08
Matt Evans <mattevans98@gmail.com>
2023-04-08 00:47:19 -0500

    Added S3 with routing to S4 and back to S2

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement } from 'react';
 import './App.scss';
 import Scenario1 from "./scenario1/Scenario1";
 import Scenario2 from "./scenario2/Scenario2";
+import Scenario3 from "./scenario3/Scenario3";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = (): ReactElement => {
@@ -10,6 +11,7 @@ const App = (): ReactElement => {
             <Routes>
                 <Route path="/" element={ <Scenario1/> }/>
                 <Route path="/scenario2" element={ <Scenario2/> }/>
+                <Route path="/scenario3" element={ <Scenario3/> }/>
             </Routes>
         </Router>
     );
