470d2bb7b0038c3b656373e1f680d9f1f7ddc1a8
Matt Evans <mattevans98@gmail.com>
2023-04-07 23:42:09 -0500

    Added router with landing page route

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,16 @@ import React, { ReactElement, useState } from 'react';
 import './App.scss';
 import Scenario1 from "./scenario1/Scenario1";
 import Scenario2 from "./scenario2/Scenario2";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = (): ReactElement => {
-    const [scenario, setScenario] = useState(-1);
-    const scenarioArr: ReactElement[] = [<Scenario1/>, <Scenario2/>];
-
     return (
-        <div className="App">
-            <header className="App-header">
-                Appoint-o-matic 9000 Prototypes
-            </header>
-            <main className="App-content">
-                <div className="scenario-selector">
-                    <h3>Select a scenario</h3>
-                    <div className="scenario-buttons">
-                        <button onClick={ () => setScenario(scenario !== 0 ? 0 : -1) }>Scenario 1</button>
-                        <button onClick={ () => setScenario(scenario !== 1 ? 1 : -1) }>Scenario 2</button>
-                        <button>Scenario 3</button>
-                        <button>Scenario 4</button>
-                        <button>Scenario 5</button>
-                    </div>
-                    <div className="scenario-display">
-                        { scenario === -1 ? <></> : scenarioArr[scenario] }
-                    </div>
-                </div>
-            </main>
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={ <Scenario1/> }/>
+                <Route path="/scenario2" element={ <Scenario2/> }/>
+            </Routes>
+        </Router>
     );
 }
 
